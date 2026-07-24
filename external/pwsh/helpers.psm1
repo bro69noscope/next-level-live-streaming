3e42733c5546921926b9cec7bd5f227507e546ed
Loïc Villet <villet.loic@gmail.com>
2026-07-24 21:14:58 +0200

    feat(ext: pwsh): add throw context helper

---
+++ external/pwsh/helpers.psm1
@@ -0,0 +1,17 @@
+function Write-ThrowContext {
+  <#
+    .SYNOPSIS
+        Writes the throwing file and full call stack to host. Call this
+        immediately before any `throw` to get diagnostic context in the console.
+    #>
+  [CmdletBinding()]
+  param()
+
+  Write-Host "Error thrown from: $PSCommandPath" -ForegroundColor Red
+  Write-Host "Call stack:" -ForegroundColor Red
+  Get-PSCallStack | ForEach-Object {
+    Write-Host "  at $($_.FunctionName) in $($_.ScriptName):$($_.ScriptLineNumber)" -ForegroundColor DarkYellow
+  }
+}
+
+Export-ModuleMember -Function Write-ThrowContext

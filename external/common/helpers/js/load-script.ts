e112f625811b9cb52e61ddcc95e87e53ec396df0
Loïc Villet <villet.loic@gmail.com>
2026-08-23 11:18:40 +0200

    chore(ext: helpers: js): removed unused module

--- external/common/helpers/js/load-script.ts
+++
@@ -1,9 +0,0 @@
-export function loadScript(src: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement("script");
-    script.src = src;
-    script.onload = () => resolve();
-    script.onerror = () => reject(new Error("failed to load " + src));
-    document.head.appendChild(script);
-  });
-}

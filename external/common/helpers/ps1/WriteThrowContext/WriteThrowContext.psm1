5ee4b2c9bef38cf353b55d4c5799763fa622ce64
Loïc Villet <villet.loic@gmail.com>
2026-09-04 12:48:27 +0200

    feat(ext: helpers: ps1): Make Write-ThrowContext friendlier
    - wooh so good, bold output and all

--- external/common/helpers/ps1/WriteThrowContext/WriteThrowContext.psm1
+++ external/common/helpers/ps1/WriteThrowContext/WriteThrowContext.psm1
@@ -6,10 +6,20 @@ function Write-ThrowContext {
     #>
   [CmdletBinding()]
   param()
+  $callStack = Get-PSCallStack
+  $caller = $callStack[1]
+  $callerFile = Split-Path -Leaf $caller.ScriptName
 
-  Write-Host "  Error thrown from: $PSCommandPath" -ForegroundColor Red
+  $message = ("Error thrown from: $($caller.FunctionName) in " + `
+      "$($callerFile):$($caller.ScriptLineNumber)")
+
+  $bold = ("**$message**" | ConvertFrom-Markdown `
+      -AsVT100EncodedString).VT100EncodedString.Replace("`n", '')
+
+  Write-Host ""
+  Write-Host $bold -ForegroundColor Red
   Write-Host "  Call stack:" -ForegroundColor Red
-  Get-PSCallStack | ForEach-Object {
+  $callStack | Select-Object -Skip 1 | ForEach-Object { # 1 is always this module function, skipped
     Write-Host "    at $($_.FunctionName) in $($_.ScriptName):$($_.ScriptLineNumber)" `
       -ForegroundColor Red
   }
